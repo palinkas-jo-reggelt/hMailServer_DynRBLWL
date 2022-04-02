d23b7fde62e4bf5e015c4f61232843cf65aa64ac
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2022-04-02 19:53:56 -0400

    added version

--- foot.php
+++ foot.php
@@ -1,6 +1,15 @@
 
-	<br><br>
-	<div class="footer"></div>
+	<br>
+	<div class="footer">
+		Copyright 2022 Pálinkás Jó Reggelt
+<?php 
+(int)$versionGitHub = file_get_contents('https://raw.githubusercontent.com/palinkas-jo-reggelt/hMailServer-IDS-Viewer/main/VERSION');
+(int)$versionLocal = file_get_contents('VERSION');
+if ($versionLocal < $versionGitHub) {
+	echo "<br><br>Upgrade to version ".$versionGitHub." available at <a href='https://github.com/palinkas-jo-reggelt/hMailServer-IDS-Viewer'>GitHub</a>";
+}
+?>
+	</div>
 
 </div> <!-- end WRAPPER -->
 </body>
